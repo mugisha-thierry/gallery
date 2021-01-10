aa2028e6ee3a6f04dc040be4e4f1ffb83c34d57e
Mugisha Thierry <thierrymugis@gmail.com>
2021-01-10 11:11:30 +0200

    Added javascript

---
+++ static/script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied: " + copyText.value;
+  }
