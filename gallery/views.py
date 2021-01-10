880db6967d6560944d8c45257755dba5beb7fc08
Mugisha Thierry <thierrymugis@gmail.com>
2021-01-10 10:39:32 +0200

    Added routes codes

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+from .models import Image, Location, Category
 
 # Create your views here.
+def first(request):
+    images = Image.objects.all()
+    locations = Location.get_locations()
+    category = Category.objects.all()
+    context = {'images':images, 'locations': locations, 'category':category}
+    return render(request, 'home.html', context )
+
+
+def image_location(request, location):
+    images = Image.filter_by_location(location)
+    context = {'images':images}
+    return render(request,'location.html',context)
