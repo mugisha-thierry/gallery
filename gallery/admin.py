59fa9e58b6034670a5c7cd94e2df99f279ce52fe
Mugisha Thierry <thierrymugis@gmail.com>
2021-01-10 11:11:00 +0200

    Added models to admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Image,Category,Location
 # Register your models here.
+
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image)
+
+
