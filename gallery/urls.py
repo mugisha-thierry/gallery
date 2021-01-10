3b065ca0bd752fa6dede5e3561718cb9ae43b8bd
Mugisha Thierry <thierrymugis@gmail.com>
2021-01-10 10:27:21 +0200

    Created urls.py file

---
+++ gallery/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.first,name = 'first'),
+    path('location/<location>',views.image_location,name = 'location'),
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
